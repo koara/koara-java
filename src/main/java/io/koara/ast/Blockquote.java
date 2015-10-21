cb0e0f78bf5e366fe864183d3bb4209643b480f0
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-10-21 07:28:17 +0200

    remove comments

--- src/main/java/io/koara/ast/Blockquote.java
+++ src/main/java/io/koara/ast/Blockquote.java
@@ -1,4 +1,3 @@
-/* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package io.koara.ast;
 
 import io.koara.KoaraVisitor;
