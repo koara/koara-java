d6d417d075f4bb35109b1bfd5c9f420622e4f03f
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-01-10 14:29:30 +0100

    add charstream tests

---
+++ src/test/java/io/koara/CharStreamTest.java
@@ -0,0 +1,46 @@
+package io.koara;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CharStreamTest {
+
+	private CharStream cs;
+	
+	@Before
+	public void setUp() {
+		cs = new CharStream(new StringReader("abcd"));
+	}
+	
+	@Test
+	public void testBeginToken() throws Exception {
+		assertEquals('a', cs.beginToken());
+		assertEquals(1, cs.getBeginColumn());
+		assertEquals(1, cs.getBeginLine());
+		assertEquals(1, cs.getEndColumn());
+		assertEquals(1, cs.getEndColumn());
+	}
+	
+	@Test
+	public void testReadChar() throws Exception {
+		assertEquals('a', cs.readChar());
+		assertEquals('b', cs.readChar());
+		assertEquals('c', cs.readChar());
+		assertEquals('d', cs.readChar());
+	}
+	
+	@Test(expected=IOException.class)
+	public void testReadCharTillEof() throws Exception {
+		cs.readChar();
+		cs.readChar();
+		cs.readChar();
+		cs.readChar();
+		cs.readChar();
+	}
+	
+}
