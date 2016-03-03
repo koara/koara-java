64ce86b91ddf3a2b001473cdedcd61817243f6db
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-03-03 22:45:55 +0100

    remove else

--- src/main/java/io/koara/CharStream.java
+++ src/main/java/io/koara/CharStream.java
@@ -125,9 +125,8 @@ public class CharStream {
     public String getImage() {
         if (bufpos >= tokenBegin) {
             return new String(buffer, tokenBegin, bufpos - tokenBegin + 1);
-        } else {
-            return new String(buffer, tokenBegin, bufsize - tokenBegin) + new String(buffer, 0, bufpos + 1);
-        }
+        } 
+        return new String(buffer, tokenBegin, bufsize - tokenBegin) + new String(buffer, 0, bufpos + 1);
     }
 
     public int getEndColumn() {
